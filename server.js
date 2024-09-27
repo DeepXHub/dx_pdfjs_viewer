e689098bf3806b17d57fd944da3a2426ab06e0d1
Borys Gelfandbein <borys.gelfandbein@deepxhub.com>
2024-09-27 21:12:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const PORT = process.env.PORT || 10000;
 // Regex to allow any subdomain of deepxhub.com
 const allowedOriginPattern = /^https:\/\/([a-z0-9-]+\.)*deepxhub\.com$/;
 
+// Regex to check referer from deepxhub.com
+const allowedRefererPattern = /^https:\/\/([a-z0-9-]+\.)*deepxhub\.com$/;
+
 // List of specific allowed origins
 const allowedOrigins = [
     'https://dx-pdfjs-viewer-e82kw.ondigitalocean.app',
-    'http://dx-pdfjs-viewer-e82kw.ondigitalocean.app',
     'http://localhost:3000'
     // Add more domains as needed
 ];
@@ -19,6 +21,13 @@ const allowedOrigins = [
 // CORS middleware to handle origin checking
 app.use((req, res, next) => {
     const origin = req.headers.origin;
+    const referrer = req.get('referer') || '';
+
+    // If referer matches deepxhub.com, skip CORS check
+    if (allowedRefererPattern.test(referrer)) {
+        console.log(`Referer ${referrer} matched deepxhub.com. Skipping CORS check.`);
+        return next(); // Skip CORS check and continue to the next middleware
+    }
 
     // Check if the incoming origin matches the allowed pattern or is in the allowedOrigins list
     const isAllowed = allowedOrigins.includes(origin) || allowedOriginPattern.test(origin);
@@ -35,7 +44,7 @@ app.use((req, res, next) => {
 // Middleware to log incoming requests
 app.use((req, res, next) => {
     const clientIp = req.ip;
-    const referrer = req.get('Referrer') || 'No referrer';
+    const referrer = req.get('Referer') || 'No referrer';
     console.log(`[dx-pdfjs-viewer] GET request made to: ${req.path} from IP: ${clientIp}, Referrer: ${referrer}`);
     console.log(`[dx-pdfjs-viewer] Full request URL: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
     next();
